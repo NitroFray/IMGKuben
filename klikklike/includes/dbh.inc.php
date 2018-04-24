0c51c20f83ac0d0d54f01a01eb5273f7461c1537
NitroFray <martih1909@osloskolen.no>
2018-04-24 09:07:34 +0200

    s
    SEND DA FUCKER

--- klikklike/includes/dbh.inc.php
+++ klikklike/includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbServername = "http://www.informatikk5.net";
-$dbUsername = "inforsod_martin2STT";
-$dbPassword = "ca38_vs37_n2";
-$dbName = "inforsod_db9";
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "test";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
